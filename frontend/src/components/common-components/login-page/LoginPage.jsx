abd0d5e5931480337501bee84f4b42d60d9f749f
Himanshu Vishwakarma <himanshu77jangid@gamil.com>
2026-05-08 19:52:48 +0530

    feat: update password visibility toggle to use button for improved accessibility

--- frontend/src/components/common-components/login-page/LoginPage.jsx
+++ frontend/src/components/common-components/login-page/LoginPage.jsx
@@ -110,12 +110,14 @@ const LoginPage = () => {
                     type={showPassword ? "text" : "password"} 
                     label="Password" 
                   />
-                  <div
-                    className="absolute bottom-0 right-3 h-11 flex items-center text-gray-500 cursor-pointer z-10"
+                  <button
+                    type="button"
+                    className="absolute right-3 top-[38px] bottom-0 flex items-center text-gray-400 hover:text-gray-600"
                     onClick={() => setShowPassword(!showPassword)}
+                    tabIndex={-1}
                   >
-                    {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
-                  </div>
+                    {showPassword ? <EyeOff size={18} /> : <Eye size={18} />}
+                  </button>
                 </div>
 
                 {loginError && (
